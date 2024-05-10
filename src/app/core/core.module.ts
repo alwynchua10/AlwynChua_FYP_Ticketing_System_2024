c96a9ea987edb75485746c9543648fffc730a287
Alwyn <alwynchuaz@gmail.com>
2024-05-10 09:04:06 +0800

    Test

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderContainerComponent } from './header-container/header-container.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, HeaderContainerComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, HeaderContainerComponent],
 })
 export class CoreModule {}
